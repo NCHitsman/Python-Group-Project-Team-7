842ccb0c5c600be35363e33088626f8d3c5bdd60
enomilan <andrewmusta@gmail.com>
2021-06-21 17:49:32 -0500

    Event table

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-
-
 const ProtectedRoute = props => {
   const user = useSelector(state => state.session.user);
   return (
