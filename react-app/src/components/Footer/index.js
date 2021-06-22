561a3503d47cad108e4be452455a2e841804b0d6
leonhardt <jacob.leonhardt649@gmail.com>
2021-06-22 11:07:40 -0500

    footer added and styled

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,18 +6,24 @@ const Footer = () => {
 
     return (
         <footer>
+            <div className="footer-top">
             <div className="footer-left">
                 <a href="/"><img src={logo} /></a>
             </div>
             <div className="footer-middle">
-
+                <h6>About Robinhoop</h6>
+                <p>Robinhoop is a simulated basketball stock market, not an actual stock market. No real currency is won, lost, or exchanged by using this app. It's mostly just for fun.</p>
             </div>
             <div className="footer-right">
                 <h6>Site by:</h6>
-                <p>Andrew Musta,<br/>
-                    Noah Carmichael-Hitsman,<br/>
-                    Nathan Mac,<br/>
-                    Jacob Leonhardt</p>
+                <p>Andrew Musta <a href="https://github.com/enomilan"><ion-icon name="logo-github"></ion-icon></a><br/>
+                    Nathan Mac <a href="https://github.com/nathan-mac"><ion-icon name="logo-github"></ion-icon></a> <a href="https://www.linkedin.com/in/nathan-m-68b60582/"><ion-icon name="logo-linkedin"></ion-icon></a><br/>
+                    Noah Carmichael-Hitsman <a href="https://github.com/NCHitsman"><ion-icon name="logo-github"></ion-icon></a> <a href="https://www.linkedin.com/in/noah-carmichael-hitsman-b024a1203/"><ion-icon name="logo-linkedin"></ion-icon></a><br/>
+                    Jacob Leonhardt <a href="https://github.com/jacobleonhardt"><ion-icon name="logo-github"></ion-icon></a> <a href="https://www.linkedin.com/in/jacob-leonhardt-b19067ba/"><ion-icon name="logo-linkedin"></ion-icon></a></p>
+            </div>
+            </div>
+            <div className="bottom-bar">
+                <hr/>
                 <p>©2021 Robinhoop</p>
             </div>
         </footer>
--- react-app/src/components/Home/LoggedOutHome.js
+++ react-app/src/components/Home/LoggedOutHome.js
@@ -12,7 +12,7 @@ const LoggedOutHome = () => {
                         <h1 className='signup__info__title__text'>Robinhoop Investing</h1>
                         <p className='signup__info__body__text'>Robinhoop is a simulated clone of the Robinhood Stock Trading app, but focused on Basketball. With Robinhoop, users can invest in their favorite basketball teams (or the ones that are actually winning games). Robinhoop users buy, sell, and keep tabs on NBA basketball teams to see which ones they want to buy stock in, or sell their stock in.</p>
                         <button>Get in the Game</button>
-                        <a className='signup__info__disclaimer__text' href="#"><span class="material-icons">account_circle</span> Play as Demo User</a>
+                        <a className='signup__info__disclaimer__text' href="#"><ion-icon name="person-circle-outline"></ion-icon> Play as Demo User</a>
                     </div>
                 </div>
             </div>
@@ -22,8 +22,8 @@ const LoggedOutHome = () => {
                         <img src={graph} />
                     </div>
                     <div className='solo__info__text__cont'>
-                        <h2 className='solo__info__title__text'>Heads Up</h2>
-                        <p className='solo__info__body__text'>Robinhoop is a simulated basketball stock market, not an actual stock market. No real currency is won, lost, or exchanged by using this app. It's mostly just for fun.</p>
+                        <h2 className='solo__info__title__text'>Lorum Ipsum</h2>
+                        <p className='solo__info__body__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                 </div>
             </div>
