a4ba2a93d8ae6383b1c3f0db259d47fe1eb8c122
leonhardt <jacob.leonhardt649@gmail.com>
2021-06-22 15:39:23 -0500

    starting on watchlist

---
+++ react-app/src/components/Watchlist/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { useSelector } from "react-redux"
+import TeamStockCard from '../TeamStockCard'
+
+
+export const watchlist = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
