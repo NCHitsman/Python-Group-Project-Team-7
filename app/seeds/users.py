587c16a3dcddb7bf3b6ccedaaa866e5aa2c0d5d5
Nathan <nathanmac777@hotmail.com>
2021-06-23 18:20:10 +0000

    fix formatting

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,11 +1,14 @@
 from app.models import db, User
 
+
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
-
+    demo = User(
+        username='Demo',
+        email='demo@aa.io',
+        password='password'
+    )
 
     demo1 = User(username='User', email='user@email.io', password='password')
     demo2 = User(username='None', email='none@noemail.io', password='password')
@@ -20,6 +23,8 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
