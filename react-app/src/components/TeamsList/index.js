a937ff73077af49e96544523839d182c7924557f
Nathan <nathanmac777@hotmail.com>
2021-06-23 20:00:14 +0000

    cleanup pt2

--- react-app/src/components/TeamsList/index.js
+++ react-app/src/components/TeamsList/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
 import placeholder from "../../images/robinhoop-background-ball.jpg";
 import "./teams-list.css"
 
