d18afc48ac8ef90bb8ff1a0fd01af4487c0b79c3
Nathan <nathanmac777@hotmail.com>
2021-06-23 23:32:49 +0000

    stock page add content div wrapper

--- react-app/src/components/StockPage/index.js
+++ react-app/src/components/StockPage/index.js
@@ -33,54 +33,56 @@ const StockPage= ({currentUser}) => {
 
     }
 
-        return (
-            <div className="container">
-                <div className='parent__cont'>
-                    <div className='graph__cont'>
-                        <GraphCanvas history={history}/>
-                    </div>
-                    <div className='info__buy__sell__parent__cont'>
-
-                        <div className='info__cont'>
-                            <div className='info__stock__name'>{stock?.name}</div>
-                            <div className='info__stock__conference'>{stock?.conference}</div>
-                            <div className='info__stock__short_name'>{stock?.short_name}</div>
-                            <div className='info__stock__price'>{stock?.price}</div>
-                            <div className='info__stock__icon'></div>
+    return (
+            <div className="content">
+                <div className="container">
+                    <div className='parent__cont'>
+                        <div className='graph__cont'>
+                            <GraphCanvas history={history}/>
                         </div>
+                        <div className='info__buy__sell__parent__cont'>
 
-                        <div className='buy__cont'>
-                            <div className='buy__title'>Buy {stock?.name}</div>
-                            <input
-                            type='number'
-                            className='buy__quant__input'
-                            onChange={(e) => setBuyQuant(e.target.value)}
-                            value={buyQuant}
-                            >
-                            </input>
-                            <div className='buy__price__total'>${buyQuant * stock?.price}</div>
-                            <button
-                            className='buy__button'
-                            onClick={(e) => buyHandler()}
-                            >Buy Now</button>
+                            <div className='info__cont'>
+                                <div className='info__stock__name'>{stock?.name}</div>
+                                <div className='info__stock__conference'>{stock?.conference}</div>
+                                <div className='info__stock__short_name'>{stock?.short_name}</div>
+                                <div className='info__stock__price'>{stock?.price}</div>
+                                <div className='info__stock__icon'></div>
+                            </div>
+
+                            <div className='buy__cont'>
+                                <div className='buy__title'>Buy {stock?.name}</div>
+                                <input
+                                type='number'
+                                className='buy__quant__input'
+                                onChange={(e) => setBuyQuant(e.target.value)}
+                                value={buyQuant}
+                                >
+                                </input>
+                                <div className='buy__price__total'>${buyQuant * stock?.price}</div>
+                                <button
+                                className='buy__button'
+                                onClick={(e) => buyHandler()}
+                                >Buy Now</button>
+                            </div>
+
+                            <div className='sell__cont'>
+                                <div className='sell__title'>Sell {stock?.name}</div>
+                                <input
+                                type='number'
+                                className='sell__quant__select'
+                                onChange={(e) => setSellQuant(e.target.value)}
+                                value={sellQuant}
+                                >
+                                </input>
+                                <div className='sell__price__total'>${sellQuant * stock?.price}</div>
+                                <button
+                                className='sell__button'
+                                onClick={(e) => sellHandler()}
+                                >Sell Now</button>
+                            </div>
+
                         </div>
-
-                        <div className='sell__cont'>
-                            <div className='sell__title'>Sell {stock?.name}</div>
-                            <input
-                            type='number'
-                            className='sell__quant__select'
-                            onChange={(e) => setSellQuant(e.target.value)}
-                            value={sellQuant}
-                            >
-                            </input>
-                            <div className='sell__price__total'>${sellQuant * stock?.price}</div>
-                            <button
-                            className='sell__button'
-                            onClick={(e) => sellHandler()}
-                            >Sell Now</button>
-                        </div>
-
                     </div>
                 </div>
             </div>
