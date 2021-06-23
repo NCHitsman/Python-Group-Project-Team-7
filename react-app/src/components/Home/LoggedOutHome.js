e614ce28ed83ae694f22019e3823fecbc11d8421
leonhardt <jacob.leonhardt649@gmail.com>
2021-06-23 10:53:59 -0500

    scores mobile adjustment and nav active style

--- react-app/src/components/Home/LoggedOutHome.js
+++ react-app/src/components/Home/LoggedOutHome.js
@@ -1,8 +1,22 @@
-import React from "react"
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../store/session";
+
 import graph from "../../images/graph.png";
 import "./home.css"
 
 const LoggedOutHome = () => {
+    const dispatch = useDispatch();
+    const [errors, setErrors] = useState([]);
+
+    const onDemoLogin = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login("demo@aa.io", "password"));
+        if (data.errors) {
+          setErrors(data.errors);
+        }
+      }
+
     return (
         <div className="content">
           <div className='parent__cont'>
@@ -11,8 +25,10 @@ const LoggedOutHome = () => {
                     <div className='signup__info__text__cont'>
                         <h1 className='signup__info__title__text'>Robinhoop Investing</h1>
                         <p className='signup__info__body__text'>Robinhoop is a simulated clone of the Robinhood Stock Trading app, but focused on Basketball. With Robinhoop, users can invest in their favorite basketball teams (or the ones that are actually winning games). Robinhoop users buy, sell, and keep tabs on NBA basketball teams to see which ones they want to buy stock in, or sell their stock in.</p>
-                        <button>Get in the Game</button>
-                        <a className='signup__info__disclaimer__text' href="/"><ion-icon name="person-circle-outline"></ion-icon> Play as Demo User</a>
+                        <a href="/join"><button>Join the Game</button></a>
+                        <form class="demo-link" onSubmit={onDemoLogin}>
+                            <button type="submit"><ion-icon name="person-circle-outline"></ion-icon> Play as Demo User</button>
+                        </form>
                     </div>
                 </div>
             </div>
