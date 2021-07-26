e6b4867f39ab0ae27744f43cec0a49428604e96f
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-26 11:56:28 -0500

    redirecting to 404 when not valid stock

--- react-app/src/components/StockPage/index.js
+++ react-app/src/components/StockPage/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import {useParams} from 'react-router-dom'
+import {useHistory, useParams} from 'react-router-dom'
 import StockPageInfo from './StockPageInfo'
 
 import "./stockpage.css"
 import GraphCanvas from '../Graph'
 
 const StockPage= () => {
+
+    const redirectTo = useHistory()
+
     const {stockId} = useParams()
+
+    if (stockId > 30) {
+        redirectTo.push('/404')
+    }
 
 
     const history = useSelector((state) => state.stocks.history[stockId])
@@ -15,7 +22,7 @@ const StockPage= () => {
         return (
             <div className="container">
                 <GraphCanvas history={history} stockId={stockId}/>
-                <StockPageInfo stockId={stockId}/>
+                <StockPageInfo stockId={stockId > 30 ? 1 : stockId}/>
             </div>
     )
 }
